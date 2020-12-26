8821d06ca8a123fd067b3f983692df39a39e87fb
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-26 13:23:13 +0100

    Adjust the endpoint to handle json

--- springboot-kafka-producer/src/main/java/com/nexthink/mnt/springbootkafkaproducer/resource/UserResource.java
+++ springboot-kafka-producer/src/main/java/com/nexthink/mnt/springbootkafkaproducer/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.nexthink.mnt.springbootkafkaproducer.resource;
 
+import com.nexthink.mnt.springbootkafkaproducer.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("kafka")
 public class UserResource {
 
+    // @Autowired takes care of the whole configuration
+    // THis kafka was not bootstrapped because it is running locally
     @Autowired
-    KafkaTemplate<String, String> kafkaTemplate;
+    KafkaTemplate<String, User> kafkaTemplate;
+    // Name if the kafka topic
     private static final String TOPIC = "kafka_topic";
 
-    @GetMapping("/publish/{message}")
-    public String kafkaPublishMessage(@PathVariable("message") final String message) {
+    // Handle REST GET request
+    @GetMapping("/publish/{name}")
+    public String kafkaPublishMessage(@PathVariable("message") final String name) {
 
-        kafkaTemplate.send(TOPIC, message);
+        // Create a user with whatever name provided in the path
+        User user = new User(name, "Mantis", 500000L);
+        // Publish to kafka
+        kafkaTemplate.send(TOPIC, user);
 
         return "published successfully";
     }
