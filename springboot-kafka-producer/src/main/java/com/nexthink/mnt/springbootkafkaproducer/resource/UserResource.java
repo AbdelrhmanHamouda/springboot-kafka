e254e946195be44bc22a7c6e8951847276fe4e45
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-26 12:49:09 +0100

    added kafka configuration to endpoint

--- springboot-kafka-producer/src/main/java/com/nexthink/mnt/springbootkafkaproducer/resource/UserResource.java
+++ springboot-kafka-producer/src/main/java/com/nexthink/mnt/springbootkafkaproducer/resource/UserResource.java
@@ -1,4 +1,25 @@
 package com.nexthink.mnt.springbootkafkaproducer.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
 public class UserResource {
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC = "kafka_topic";
+
+    @GetMapping("/publish/{message}")
+    public String post(@PathVariable("message") final String message) {
+        
+        kafkaTemplate.send(TOPIC, message);
+
+        return "published successfully";
+    }
 }
