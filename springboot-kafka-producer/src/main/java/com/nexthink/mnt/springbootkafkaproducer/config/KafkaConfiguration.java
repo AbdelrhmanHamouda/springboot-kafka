295f618876078ae0a69aac5872699dcedfb783e4
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-26 13:36:23 +0100

    Complete the kafka configuration for json

---
+++ springboot-kafka-producer/src/main/java/com/nexthink/mnt/springbootkafkaproducer/config/KafkaConfiguration.java
@@ -0,0 +1,39 @@
+package com.nexthink.mnt.springbootkafkaproducer.config;
+
+import com.nexthink.mnt.springbootkafkaproducer.model.User;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+    // * How to set a configuration for a special publish type (e.g. json in this case)
+
+    @Bean
+    public ProducerFactory<String, User> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        // Point to the kafka server
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.0.0.1:9092");
+        // Key configuration
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        // Value configuration
+        // * This will tell kafka that json configuration is to be used for values
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, User> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
