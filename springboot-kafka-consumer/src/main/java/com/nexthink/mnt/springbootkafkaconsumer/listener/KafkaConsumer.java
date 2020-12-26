2f098f7e0585a6da603b8dfe3de01655a8152d6d
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-26 19:12:44 +0100

    Implement the consumer

--- springboot-kafka-consumer/src/main/java/com/nexthink/mnt/springbootkafkaconsumer/listener/KafkaConsumer.java
+++ springboot-kafka-consumer/src/main/java/com/nexthink/mnt/springbootkafkaconsumer/listener/KafkaConsumer.java
@@ -1,4 +1,19 @@
 package com.nexthink.mnt.springbootkafkaconsumer.listener;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+
+@Service
 public class KafkaConsumer {
+
+    // Which topic this consumer will subscribe to
+    @KafkaListener(topics = "kafka_topic", groupId = "group_id")
+    // print the received message
+    public void consume(String message) {
+        System.out.println(
+                MessageFormat.format("Received Kafka message is:\n{0}", message)
+        );
+    }
 }
