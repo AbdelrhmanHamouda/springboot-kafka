9cbdf4a86706c3adbeb2b4949a760d7cbcaec141
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-27 12:44:19 +0100

    implement KafkaListener for User json

--- springboot-kafka-consumer/src/main/java/com/nexthink/mnt/springbootkafkaconsumer/listener/KafkaConsumer.java
+++ springboot-kafka-consumer/src/main/java/com/nexthink/mnt/springbootkafkaconsumer/listener/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.nexthink.mnt.springbootkafkaconsumer.listener;
 
+import com.nexthink.mnt.springbootkafkaconsumer.model.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,16 @@ public class KafkaConsumer {
     public void consume(String message) {
         System.out.println(
                 MessageFormat.format("Received Kafka message is:\n{0}", message)
+        );
+    }
+
+    // Which topic this consumer will subscribe to
+    // print the received message
+    @KafkaListener(topics = "kafka_topic", group = "group_json",
+            containerFactory = "kafkaListenerContainerFactoryUserJson")
+    public void consumeUserJson(User user) {
+        System.out.println(
+                MessageFormat.format("Received Kafka message is:\n{0}", user)
         );
     }
 }
