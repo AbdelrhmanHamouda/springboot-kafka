11c12d0c1e782d9b7f2ec7a7573afacf8df415d4
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-27 12:23:29 +0100

    implement the User POJO for json

--- springboot-kafka-consumer/src/main/java/com/nexthink/mnt/springbootkafkaconsumer/model/User.java
+++ springboot-kafka-consumer/src/main/java/com/nexthink/mnt/springbootkafkaconsumer/model/User.java
@@ -1,4 +1,27 @@
 package com.nexthink.mnt.springbootkafkaconsumer.model;
 
 public class User {
+    private String name;
+    private String dept;
+
+    public User(String name, String dept) {
+        this.name = name;
+        this.dept = dept;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
 }
